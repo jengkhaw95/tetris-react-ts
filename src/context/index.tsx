e5947a2bd19fc34c621545d3286f69b40115fad3
Jeng Khaw <jengkhaw95@gmail.com>
2023-04-08 22:30:24 +0800

    check if keyboard is available

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,14 +1,26 @@
 import { PropsWithChildren, useContext, useEffect, useState } from 'react'
 import { createContext } from 'react'
 
-const globalContext = createContext<{ cellSize: number }>({ cellSize: 0 })
+const DEFAULT_CONTEXT = { cellSize: 0, isKeyboardAvailable: true }
+
+const globalContext = createContext<{
+  cellSize: number
+  isKeyboardAvailable: boolean
+}>(DEFAULT_CONTEXT)
 
 export const useGlobalContext = () => useContext(globalContext)
 
 export function GlobalProvider({ children }: PropsWithChildren) {
-  const [cellSize, setCellSize] = useState(0)
+  const [cellSize, setCellSize] = useState(DEFAULT_CONTEXT['cellSize'])
+  const [isKeyboardAvailable, setIsKeyboardAvailable] = useState(
+    DEFAULT_CONTEXT['isKeyboardAvailable']
+  )
 
   useEffect(() => {
+    const handleTouchStart = () => {
+      setIsKeyboardAvailable(false)
+    }
+
     const updateCellSize = () => {
       // Dimension -  window.innerHeight, window.innerWidth
       const cs = Math.min(24, Math.max(10, window.innerHeight / 40))
@@ -17,14 +29,16 @@ export function GlobalProvider({ children }: PropsWithChildren) {
 
     updateCellSize()
     window.addEventListener('resize', updateCellSize)
+    window.addEventListener('touchstart', handleTouchStart)
 
     return () => {
       window.removeEventListener('resize', updateCellSize)
+      window.removeEventListener('touchstart', handleTouchStart)
     }
   }, [])
 
   return (
-    <globalContext.Provider value={{ cellSize }}>
+    <globalContext.Provider value={{ cellSize, isKeyboardAvailable }}>
       {children}
     </globalContext.Provider>
   )
